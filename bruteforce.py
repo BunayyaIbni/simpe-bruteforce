0ca4acc04b4ecdd9190f7dbfa1d0713a08f2f673
Ibni Hasyim <76955597+BunayyaIbni@users.noreply.github.com>
2022-08-12 19:09:27 +0700

    Update bruteforce.py

--- bruteforce.py
+++ bruteforce.py
@@ -1,6 +1,6 @@
 import time
 import requests
-url = 'https://tubes-sa.000webhostapp.com/login.php'   #untuk koneksi mengambil request nantinya
+url = 'http://localhost/stragal/login.php'   #untuk koneksi mengambil request nantinya
 
 #list kata kunci untuk dicari username dan passwordnya
 users = ['user', 'admin', 'password', 'bandung', 'katasandi', 'telkom', 'sukapura1', 'sukabirus3', 'sInerGiBanguNNegri', 'kitaBisa', 'user1', 'admin1', 'user3', 'admin2', 'akukeren1*']
